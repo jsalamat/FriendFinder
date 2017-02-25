cae71b7d8b3e1d4e7fbb25a5849dc1e470304e72
Jon Salamat <Jon Salamat>
2017-02-24 19:20:29 -0500

    package.json, express,body-parser,path, and server tested

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.get("/", function(req, res) {
+  res.send('everything went good');
+});
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});

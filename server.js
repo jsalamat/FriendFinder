d2896934dc7f61fc55818f5166df66bf8234d49f
Jon Salamat <Jon Salamat>
2017-02-26 22:02:36 -0500

    operate similar to demo home and survey html

--- server.js
+++ server.js
@@ -17,9 +17,16 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.get("/", function(req, res) {
-  res.send('everything went good');
+	console.log(__dirname);
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
 
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+//https://codingbootcamp.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=e711d944-d619-4736-a8bd-1c41e603d52e
+//18:00 focus os sequalize change home and survey looks
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);

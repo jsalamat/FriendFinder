90641734bc58b0d9f4ac6faa1a0125be0053bdf0
Jon Salamat <Jon Salamat>
2017-02-27 02:03:50 -0500

    route to all-friends works but need to get friend.js to hold data not server.js

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+
+var userData = [
+			{
+	    		name: "Sasha Banks",
+	    		photo: "https://pbs.twimg.com/profile_images/821747940784009216/jXgwJueL.jpg",
+	    		scores: [ 5, 1, 5, 4, 1, 3, 5, 1, 1, 1 ]
+			},{
+	    		name: "Bayley",
+	    		photo: "https://pbs.twimg.com/profile_images/803427287442092033/SMbqhEef.jpg",
+	    		scores: [ 5, 5, 1, 1, 1, 5, 1, 5, 5, 5 ]
+			},
+
+]
+
+app.get("/all-friends", function(req, res) 
+	{
+	res.json(userData)
+});
+
 app.get("/", function(req, res) {
 	console.log(__dirname);
   res.sendFile(path.join(__dirname, "app/public/home.html"));
@@ -23,6 +42,10 @@ app.get("/", function(req, res) {
 
 app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+app.post("/api/friends", function(req, res) {
+  res.send(req.body);
 });
 
 //https://codingbootcamp.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=e711d944-d619-4736-a8bd-1c41e603d52e
